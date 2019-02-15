1c193a0591c5ce4f403496932d3a3dc722d2f84d
Michelle Orwick <morwick8@gmail.com>
2019-02-14 17:48:55 -0700

    initial map

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import './App.css';
 
 class App extends Component {
     
+    componentDidMount() {
+        this.renderMap()
+    
+    }
+    
     initMap = () =>  {
-       const map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: -34.397, lng: 150.644},
-         zoom: 8
-       });
+       const map = new window.google.maps.Map(document.getElementById('map'), {
+         center: {lat: 40.34383, lng: -104.8534},
+         zoom: 14
+       })
      }
 
-  render() {
-    return (
-      <main>
-        <div id="map"></div>
-        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-    async defer></script>
-      </main>
-    );
-  }
+     renderMap = () => {
+         mapScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAx9qqXmxIZck6A-W_InVovJFhL5Eg7xRo&callback=initMap")
+         window.initMap = this.initMap
+     }
+
+       render() {
+         return (
+           <main>
+             <div id="map"></div>
+
+           </main>
+         );
+       }
+     }
+
+     function mapScript(url) {
+         var ref = window.document.getElementsByTagName("script")[0]
+         var script = window.document.createElement("script")
+         script.src = url
+         script.async = true
+         script.defer = true
+         ref.parentNode.insertBefore(script, ref)
 }
 
 export default App;
